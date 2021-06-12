4f4b33205b9aab5500ca830bf57e97c46584a678
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2021-06-12 23:45:44 +0200

    Correction des warnings pour pouvoir mettre en prod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState  } from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -6,24 +6,14 @@ import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+  
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+  
 
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
+  
 
   return (
     <>
